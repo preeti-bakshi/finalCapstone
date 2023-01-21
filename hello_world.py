c6d8d6d0195aa9e4b91ad8a01864277ec57db044
Bakshi <ameya.bakshi@capgemini.com>
2023-01-21 16:53:17 +0000

    added new file hello_world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+##
+# This is pseudo code to take user name and age as input using input() function and display the name and age
+# and also display "Hello World" .
+# Start
+# Request name and age of user using input() function.
+# Assign the values to variable name and age.
+# Display user's name along with age.
+# End of pseudo code
+##
+user_name = str(input("Enter Your Name : "))
+user_age = int(input("Enter Your Age : "))
+print(f"Hello {user_name} ! I Got Your Age as {user_age}.\n")
+print("Git Is Awesome!")
